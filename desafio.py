53bb2c82af9ebb1338cc4da770f54441d8a56a12
rcferreira232 <rafaelcferreira232@gmail.com>
2024-04-15 00:10:38 -0300

    fix: selecao apenas de clientes existentes, nao permite valores negativos para saques e depositos, e novo extrato com numero de depositos e registro dos depositos

--- desafio.py
+++ desafio.py
@@ -11,7 +11,9 @@ clientes = {
         'nome': 'João',
         'saldo': 1000,
         'limite': 500,
-        'numero_saque': 0
+        'numero_saque': 0,
+        'numero_deposito': 0,
+        'registro_deposito': []
     },
     2: {
         'nome': 'Maria',
@@ -28,13 +30,26 @@ while True:
         break
     elif opcao == 'd':
         cliente = int(input('Digite o número do cliente: '))
+        if cliente not in clientes:
+            print('Cliente não encontrado')
+            continue
         valor = float(input('Digite o valor do depósito: '))
-        clientes[cliente]['saldo'] += valor
+        if valor < 0:
+            print('Valor inválido')
+        else:
+            clientes[cliente]['saldo'] += valor
+            clientes[cliente]['numero_deposito'] += 1
+            clientes[cliente]['registro_deposito'].append(valor)
     elif opcao == 's':
         cliente = int(input('Digite o número do cliente: '))
+        if cliente not in clientes:
+            print('Cliente não encontrado')
+            continue
         valor = float(input('Digite o valor do saque: '))
         if valor > clientes[cliente]['saldo']:
             print('Saldo insuficiente')
+        elif valor < 0:
+            print('Valor inválido')
         elif valor > clientes[cliente]['limite']:
             print('Limite excedido')
         elif clientes[cliente]['numero_saque'] >= 3:
@@ -44,14 +59,22 @@ while True:
             clientes[cliente]['numero_saque'] += 1
     elif opcao == 'e':
         cliente = int(input('Digite o número do cliente: '))
+        if cliente not in clientes:
+            print('Cliente não encontrado')
+            continue
         print(f"""
         Nome: {clientes[cliente]['nome']}
         Saldo: {clientes[cliente]['saldo']}
         Limite: {clientes[cliente]['limite']}
         Número de saques: {clientes[cliente]['numero_saque']}
+        Número de depósitos: {clientes[cliente]['numero_deposito']}
+        Registro de depósitos: {clientes[cliente]['registro_deposito']}
         """)
     elif opcao == 'l':
         cliente = int(input('Digite o número do cliente: '))
+        if cliente not in clientes:
+            print('Cliente não encontrado')
+            continue
         print(f'Limite: {clientes[cliente]["limite"]}')
     else:
         print('Opção inválida')
