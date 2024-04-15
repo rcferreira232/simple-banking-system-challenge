0869ee264e7abb280f27e1be8296f6d66aaa8acd
rcferreira232 <rafaelcferreira232@gmail.com>
2024-04-14 23:41:58 -0300

    feat: add opcao e as funcionalidades de deposito, saque, extrato, limite e sair do sistema

--- desafio.py
+++ desafio.py
@@ -20,3 +20,38 @@ clientes = {
         'numero_saque': 0
     },
 }
+
+while True:
+    print(menu)
+    opcao = input('Escolha uma opção: ')
+    if opcao == 'q':
+        break
+    elif opcao == 'd':
+        cliente = int(input('Digite o número do cliente: '))
+        valor = float(input('Digite o valor do depósito: '))
+        clientes[cliente]['saldo'] += valor
+    elif opcao == 's':
+        cliente = int(input('Digite o número do cliente: '))
+        valor = float(input('Digite o valor do saque: '))
+        if valor > clientes[cliente]['saldo']:
+            print('Saldo insuficiente')
+        elif valor > clientes[cliente]['limite']:
+            print('Limite excedido')
+        elif clientes[cliente]['numero_saque'] >= 3:
+            print('Número de saques excedido')
+        else:
+            clientes[cliente]['saldo'] -= valor
+            clientes[cliente]['numero_saque'] += 1
+    elif opcao == 'e':
+        cliente = int(input('Digite o número do cliente: '))
+        print(f"""
+        Nome: {clientes[cliente]['nome']}
+        Saldo: {clientes[cliente]['saldo']}
+        Limite: {clientes[cliente]['limite']}
+        Número de saques: {clientes[cliente]['numero_saque']}
+        """)
+    elif opcao == 'l':
+        cliente = int(input('Digite o número do cliente: '))
+        print(f'Limite: {clientes[cliente]["limite"]}')
+    else:
+        print('Opção inválida')
