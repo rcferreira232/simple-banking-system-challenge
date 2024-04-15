7f691ff949e9dbcb13786dacb4a51f20ebe73693
rcferreira232 <rafaelcferreira232@gmail.com>
2024-04-14 23:23:40 -0300

    feat: criacao dos clientes do sistema

--- desafio.py
+++ desafio.py
@@ -5,3 +5,18 @@ menu = """
 [l] Limite
 [q] Sair
 """
+
+clientes = {
+    1: {
+        'nome': 'João',
+        'saldo': 1000,
+        'limite': 500,
+        'numero_saque': 0
+    },
+    2: {
+        'nome': 'Maria',
+        'saldo': 500,
+        'limite': 300,
+        'numero_saque': 0
+    },
+}
